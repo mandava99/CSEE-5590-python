eff71da1f050ac19bbe8de6e464f655dee85b65d
sekhar1926 <adisekharraju@gmail.com>
2019-02-01 20:28:59 -0600

    icp1

---
+++ icp1/source/1-2.py
@@ -0,0 +1,7 @@
+x = int(input("Enter first number"))
+y = int(input("Enter second number"))
+print("x+y is",x+y)
+print("x-y is",x-y)
+print("x*y is",x*y)
+print("x/y is",x/y)
+print("x^y is ",x**y)
---
+++ icp1/source/1.1.py
@@ -0,0 +1,13 @@
+fname = input('First Name: ')
+lname = input('Last Name: ')
+
+def reverse(str):
+    revstr = ""
+    for i in str:
+        revstr = i + revstr
+    return revstr
+
+print(reverse(fname)+" "+reverse(lname))
+
+
+
